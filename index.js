d19bbb8f2dcc627b9a2743922cb290c62f95940d
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 03:00:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function init() {
   //put any page initialization/handlebars initialization here
-}
-document.addEventListener("DOMContentLoaded", function(event) {
-  init();
-  
-  
-  Handlebars.registerPartial('recipeDetails', document.getElementById("recipe-details-partial").innerHTML);
+   Handlebars.registerPartial('recipeDetails', document.getElementById("recipe-details-partial").innerHTML);
 
 
   Handlebars.registerHelper("displayIngredient",function(ingredient){
     return Handlebars.SafeString("<p>{{quantity}} {{name}}</p>");
   });
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  init();
+  
+  
+ 
+  
+  
 });
 
 
