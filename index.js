41a11035984a966bf36f68fc1bf0ab9e5d62ae81
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 03:46:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function createRecipe(){
   var name=document.getElementsByName("name")[0].value;
   var ingredNodes=document.getElementsByName("ingredients").values;
+  console.log(ingredNodes);
   
   
   template=Handlebars.compile(document.getElementById("recipe-template").innerHTML);
